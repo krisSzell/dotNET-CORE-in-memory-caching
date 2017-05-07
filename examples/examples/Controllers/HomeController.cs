3056fe9c5a7494ef38aadb6409515e72e11eabe9
Krzysztof Skorupa <skorupa.krzysztof1@gmail.com>
2017-05-07 08:40:02 +0200

    Simple example how to implement in memory caching in dotNET Core
    I covered here how to:
    add dependency of IMemoryCache to project,
    set cached object,
    read from cached object,
    set if object doesnt exist (TryGetValue + Set<T>),
    get or set object if does or doesnt exist (GetOrCreate<T>),
    MemoryCacheEntryOptions such as:
    AbsoluteExpiration,
    SlidingExpiration,
    Registering callback method on removing object from cache
    (description of this options is in code comments)

---
+++ examples/examples/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace examples.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMemoryCache _cache;
+
+        public HomeController(IMemoryCache cache)
+        {
+            this._cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            
+
+            // Check whenever key is already present in cache
+            //if (string.IsNullOrEmpty(_cache.Get<string>("timestamp")))
+            //{
+            //    _cache.Set<string>("timestamp", DateTime.Now.ToString());
+            //}
+            // More elegant way of the same
+            //if (!_cache.TryGetValue<string>("timestamp", out string timestamp))
+            //{
+            //    _cache.Set<string>("timestamp", DateTime.Now.ToString());
+            //}
+
+            //_cache.Set<string>("timestamp", DateTime.Now.ToString());
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            // Set expiration options of cached item
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddSeconds(10); // removed on specified date and time 
+                                                                      // (in here 20 seconds from the moment of setting this option)
+            options.SlidingExpiration = TimeSpan.FromSeconds(10);     // removed if object remains idle for specified amount of time
+            options.RegisterPostEvictionCallback(MyCallback, this);   // register a callback function when object is removed from cache
+
+            // Get existing item, if it doesnt exist create one
+            //string timestamp = _cache.GetOrCreate<string>("timestamp", entry =>
+            //{
+            //    return DateTime.Now.ToString();
+            //});
+
+            if (!_cache.TryGetValue<string>("timestamp", out string timestamp))
+            {
+                _cache.Set<string>("timestamp", DateTime.Now.ToString(), options);
+            }
+
+            ViewData["callback"] = _cache.Get<string>("callback");
+            //string timestamp = _cache.Get<string>("timestamp");
+
+            return View("About", timestamp);
+        }
+
+        private void MyCallback(object key, object value, EvictionReason reason, object state)
+        {
+            var message = $"Cache entry was removed: {reason} on {DateTime.Now.ToString()}";
+            ((HomeController)state)._cache.Set("callback", message);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
